45f54f83d4a531ed22f50851d89a8bfc4e4b345c
khovstadius <khovstadius@hotmail.com>
2016-05-31 13:15:07 +0200

    Kalles

--- Bussbolag - kalle/Bussbolag/Form1.Designer.cs
+++ Bussbolag - kalle/Bussbolag/Form1.Designer.cs
@@ -49,7 +49,6 @@
             this.button4 = new System.Windows.Forms.Button();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -60,7 +59,6 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Logga in";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -68,7 +66,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(205, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // richTextBox1
             // 
@@ -87,7 +84,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(205, 20);
             this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button2
             // 
@@ -97,7 +93,6 @@
             this.button2.TabIndex = 6;
             this.button2.Text = "Skapa konto";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -229,23 +224,12 @@
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(205, 20);
             this.textBox8.TabIndex = 22;
-            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(50, 77);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(39, 13);
-            this.label8.TabIndex = 24;
-            this.label8.Text = "Adress";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1116, 502);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.button4);
@@ -298,7 +282,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Label label8;
     }
 }
 
--- Bussbolag - sherv/Bussbolag/Form1.cs
+++ Bussbolag - sherv/Bussbolag/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,10 @@ namespace Bussbolag
 {
     public partial class Form1 : Form
     {
+        int personNr;
+        string namn;        
+        string adress;
+
         public Form1()
         {
             InitializeComponent();
@@ -31,7 +36,51 @@ namespace Bussbolag
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            namn = textBox1.Text;
+            
+        }
+    
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            personNr =  int.Parse(textBox2.Text); 
+        }
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            adress = textBox8.Text;
+        }
+        //Logga in
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectString = "SERVER= 195.178.232.16; PORT=3306; DATABASE=af9704;UID=af9704 ;PASSWORD=Shervin05;";
+            MySqlConnection connection = new MySqlConnection(connectString);
+            connection.Open();
+            MySqlCommand cmd2 = connection.CreateCommand();
+
+            ///string userPicturePath = (string)cmd1.ExecuteScalar();
+            cmd2.CommandText = "SELECT adress FROM resenar WHERE personNr ='" + personNr + "'";
+             adress = (string)cmd2.ExecuteScalar();
+            cmd2.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        //Registrera resenär
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connectString = "SERVER= 195.178.232.16; PORT=3306; DATABASE=af9704;UID=af9704 ;PASSWORD=Shervin05;";
+            MySqlConnection connection = new MySqlConnection(connectString);
+            connection.Open();
+            MySqlCommand cmd1 = connection.CreateCommand();
+            cmd1.CommandText = "insert into resenar(personNr, namn, adress) values ('" + personNr + "', '" + namn + "', '" + adress + "')";
+            //cmd1.CommandText = "INSERT INTO " + resenar + " (" + column + ") VALUES (" + value + ")";
+            cmd1.ExecuteNonQuery();
+            connection.Close();
+        }
+
+       
     }
 }
